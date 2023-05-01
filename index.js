5ca9d27e9f049e7a2213762f5f0e82f19b206604
warrenxie <xiewarren@gmail.com>
2023-05-01 01:05:02 -0700

    added comments

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// Select the required elements from the webpage
 const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
+// Add an event listener to the search button
 search.addEventListener("click", () => {
+  // Define the API key and city name from the search input
   const APIKey = "df4a073f388555fb425612942f197a44";
   const city = document.querySelector(".search-box input").value;
 
+  // If the search input is empty, exit the function
   if (city === "") return;
 
+  // Fetch the weather data from the API and parse the response as JSON
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   )
     .then((response) => response.json())
     .then((json) => {
+      // If the city is not found, display a 404 error message
       if (json.cod === "404") {
         container.style.height = "400px";
         weatherBox.style.display = "none";
@@ -24,9 +30,11 @@ search.addEventListener("click", () => {
         return;
       }
 
+      // Otherwise, hide the error message and display the weather data
       error404.style.display = "none";
       error404.classList.remove("fadeIn");
 
+      // Select the weather data elements from the webpage
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box .temperature");
       const description = document.querySelector(".weather-box .description");
@@ -35,6 +43,7 @@ search.addEventListener("click", () => {
       );
       const wind = document.querySelector(".weather-details .wind span");
 
+      // Set the weather image based on the weather condition
       switch (json.weather[0].main) {
         case "Clear":
           image.src = "images/clear.png";
@@ -51,16 +60,17 @@ search.addEventListener("click", () => {
         case "Haze":
           image.src = "images/mist.png";
           break;
-
         default:
           image.src = "";
       }
 
+      // Set the weather data elements based on the API response
       temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
       description.innerHTML = `${json.weather[0].description}`;
       humidity.innerHTML = `${json.main.humidity}%`;
       wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
+      // Display the weather data with a fade-in animation
       weatherBox.style.display = "";
       weatherDetails.style.display = "";
       weatherBox.classList.add("fadeIn");
